e43da58e539ecb479d425ed340cab78834ab7fae
Gustyper <gu.bianchi.s@gmail.com>
2023-10-13 12:49:39 -0300

    Mudei o gráfico

--- gustavo.py
+++ gustavo.py
@@ -4,36 +4,36 @@ from funcoes import filtrar_colunas, valores_unicos, contar_repeticoes, valores_
 import matplotlib.pyplot as plt
 
 def gustavo_plot(df):
-    #Agrupa as colunas escolhidas e conta a frequência 
-    grouped = df.groupby(['Area', 'Tipo de Operacao']).size()
+    df_copia = filtrar_colunas(df, "Data da Deflagracao", "Area")
+    df_copia["Mes"] = pd.to_datetime(df_copia['Data da Deflagracao']).dt.strftime('%m/%Y')
+    
+    # df_copia["Mes"] = df_copia["Mes"].map(meses)
 
-    #Cria listas como os valores na mesma ordem que eles aparecem na Series
-    areas = grouped.index.get_level_values('Area')
-    operation_types = grouped.index.get_level_values('Tipo de Operacao')
-    counts = grouped.values
+    contagem_area = df_copia["Area"].value_counts().index
 
-    #Cria listas com os valores únicos de Área e Tipo de operação
-    unique_areas = sorted(areas.unique())
-    unique_operation_types = sorted(operation_types.unique())
+    grouped = df_copia.groupby(['Area', 'Mes']).size().unstack(fill_value=0)
 
-    width = 0.8
+    grouped = grouped.loc[contagem_area[:10]]
+    # grouped.set_index('Mes', inplace=True)
 
-    fig, ax = plt.subplots()
-    bottom = np.zeros(len(unique_areas))
+    colors = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b',
+              '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2']
 
-    #Checa se um determinado tipo de operação foi executado em uma dada árae, e se foi, sua contagem é adicionada ào plot
-    for operation_type in unique_operation_types:
-        operation_counts = []
-        for area in unique_areas:
-            if (area, operation_type) in grouped.index:
-                operation_counts.append(counts[grouped.index == (area, operation_type)][0])
-            else:
-                operation_counts.append(0)
-        
-        p = ax.bar(unique_areas, operation_counts, width, label=operation_type, bottom=bottom)
-        bottom += operation_counts
+    plt.style.use("Solarize_Light2")
+    plt.figure(figsize=(14, 8))
+    plt.stackplot(grouped.columns, grouped.values, labels=grouped.index, alpha=0.8, colors=colors)
+    plt.xticks(range(1, 13), rotation=30)
+    plt.xlim(grouped.columns.min(), grouped.columns.max())
+    plt.xlabel('mês de 2022')
 
-    ax.set_title("grafico")
-    ax.legend(loc="upper right")
+    plt.ylabel('Número de operações policiais')
+    ax = plt.gca()
+    y_intervals = [100, 200, 300, 400, 500, 600, 700, 800, 900]  # Define as quantidades das linhas que cruzam o gráfico
+    for y in y_intervals:
+        ax.axhline(y=y, color='black', linestyle='--', alpha=0.3)
+    
+    plt.title('Tipos de operações durante 2022')
+    plt.legend(title='Area', title_fontsize='14', loc='upper right', bbox_to_anchor=(1.15, 1))
+    plt.show()
 
-    # plt.show()
+    return plt
--- main.py
+++ main.py
@@ -6,3 +6,5 @@ import pandas as pd
 df = funcoes.collect_data()
 
 df = clean_data(df)
+
+# gustavo_plot(df)
