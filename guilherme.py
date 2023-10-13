bf276a39e47d2c70233b75c310e1f7048e3723f2
Guilherme Buss <guibuss10@gmail.com>
2023-10-12 22:40:32 -0300

    Documentação

--- guilherme.py
+++ guilherme.py
@@ -6,6 +6,22 @@ from datacleaning import clean_data
 
 
 def make_plot_guilherme(df):
+    """Cria um gráfico de barras que exibe de forma comparativa os valores apreendidos em territórios indigenas e não-indigenas por estado.
+
+    Esta função recebe um DataFrame contendo os dados de valores apreendidos por estado e
+    gera um gráfico de barras que mostra os valores apreendidos por estado em territórios indigenas e não-indigenas.
+
+
+    Parameters
+    ----------
+    df : DataFrame
+        O DataFrame contendo os dados dos valores apreendidos por estado.
+
+    Returns
+    -------
+    Figure
+        Um objeto de figura do Matplotlib contendo o gráfico gerado.
+    """
     df_copia = filtrar_colunas(df, 'Atuacao em Territorio Indigena','Qtd Valores Apreendidos','Sigla Unidade Federativa').copy()
 
     #Remove os estados sem atuação em território indigena
