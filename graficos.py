ae33e5333b6df71096ab5e36a8019a696d57eaa1
Guilherme Buss <guibuss10@gmail.com>
2023-10-14 15:52:14 -0300

    Adicionei algumas coisas na minha visualização

--- graficos.py
+++ graficos.py
@@ -95,20 +95,21 @@ def make_plot_guilherme(df):
     df_ap_ind = df_copia[df_copia['Sigla Unidade Federativa'].isin(estados_ap_ind)]
 
     df_agrupado = df_ap_ind.groupby(['Atuacao em Territorio Indigena','Sigla Unidade Federativa'])['Qtd Valores Apreendidos'].mean().reset_index(name="Média")
-
     df_pivot = df_agrupado.pivot_table(values='Média',  columns=['Atuacao em Territorio Indigena'], index = 'Sigla Unidade Federativa', fill_value=0)
 
+    plt.style.use("Solarize_Light2")
     df_pivot.plot(kind='bar', edgecolor='white', linewidth=1)
     plt.yscale("log")
 
-    plt.title('Quantidade Apreendida em Território Indigena', fontsize=12)
     plt.xlabel('Estado (UF)', fontsize=8)
+    plt.xticks(rotation=90)
     plt.ylabel('Quantidade Apreendida (log)', fontsize=8)
     plt.grid(axis='y', linestyle='--', alpha=0.3)
 
+    plt.title('Quantidade Apreendida em Território Indigena', fontsize=12)
     legenda = ['Sim', 'Não']
     plt.legend(legenda, title='Atuação em território indigena', title_fontsize=10, fontsize=8)
-    
+
     return plt
 
 def make_gustavo_plot(df):
