792fb5ce55baf604dc71f699a959cdb60d4f6264
Vinasque <b51986@fgv.edu.br>
2023-10-14 15:16:07 -0300

    Consertei toda cagada feita

--- graficos.py
+++ graficos.py
@@ -5,6 +5,63 @@ from funcoes import collect_data, filtrar_colunas, contar_repeticoes, criar_df_g
 from datacleaning import clean_data
 from matplotlib.ticker import FuncFormatter
 
+
+def make_plot_vinicius(df):
+    """ Cria um gráfico de barras que exibe a quantidade de apreensões por estado em 2022.
+
+    Parameters
+    ----------
+    df : DataFrame
+        O DataFrame contendo os dados dos valores apreendidos por estado.
+
+    Returns
+    -------
+    Figure
+        Um objeto de figura do Matplotlib contendo o gráfico gerado.
+    """
+    # Limpeza pessoal do Dataset
+    df_estados = filtrar_colunas(df, "Sigla Unidade Federativa")
+    df_estados = contar_repeticoes(df_estados, "Sigla Unidade Federativa")
+    df_estados = df_estados.rename_axis(index = "ESTADOS")
+
+    # Plotagem do gráfico
+    regioes_cores = {"Norte": (["AC", "AM", "AP", "PA", "RO", "RR", "TO"], "green"),
+                    "Nordeste": (["AL", "BA", "CE", "MA", "PB", "PE", "PI", "RN", "SE"], "red"),
+                    "Centro-Oeste": (["DF", "GO", "MS", "MT"], "orange"),
+                    "Sudeste": (["ES", "MG", "RJ", "SP"], "yellow"),
+                    "Sul": (["PR", "RS", "SC"], (1, 0, 1))}
+
+    barras = plt.barh(df_estados.index, df_estados["QUANTIDADE"], height = 0.5)
+    total_soma = df_estados["QUANTIDADE"].sum()
+
+    for num, barra in enumerate(barras):
+        estado = df_estados.index[num]
+        for regiao, (siglas, cor) in regioes_cores.items():
+            if estado in siglas:
+                barra.set_color(cor)
+
+        largura = barra.get_width()
+        porcentagem = (largura / total_soma) * 100
+        if porcentagem < 10:
+            plt.text(largura + 10, barra.get_y() + barra.get_height() / 2, f"{porcentagem:.2f}%", ha = "left",
+                    va = "center", fontsize=8, fontweight = "bold", color = "#010101", linespacing = 0.9)
+        else:
+            plt.text(largura - 70, barra.get_y() + barra.get_height() / 2, f"{porcentagem:.2f}%", ha = "center",
+                    va = "center", fontsize=8, fontweight = "bold", color = "#010101", linespacing = 0.9)
+
+    # Legenda
+    for regiao, (siglas, cor) in regioes_cores.items():
+        plt.bar(0, 0, color = cor, label = regiao)
+    plt.legend(loc = "lower right")
+
+    # Personalização do gráfico
+    plt.xlabel("Quantidade")
+    plt.ylabel("Estados")
+    plt.title("Quantidade de Apreensões por Estado")
+    plt.grid(axis = "x", linestyle = "--", alpha=0.6)
+
+    return plt
+
 def make_plot_guilherme(df):
     """Cria um gráfico de barras que exibe de forma comparativa os valores apreendidos em territórios indigenas e não-indigenas por estado.
 
@@ -198,73 +255,17 @@ def make_plot_joao(df):
 
     return plot
 
-def make_plot_vinicius(df):
-    """ Cria um gráfico sobre a quantidade de apreensões por estado em 2022.
-
-    Returns
-    -------
-    Figure
-        Um objeto de figura do Matplotlib contendo o gráfico gerado.
-    """
-    # Limpeza pessoal do Dataset
-    df_estados = filtrar_colunas(collect_data(), "Sigla Unidade Federativa")
-    df_estados["Sigla Unidade Federativa"] = df_estados["Sigla Unidade Federativa"].str.strip()
-    df_estados = contar_repeticoes(df_estados, "Sigla Unidade Federativa")
-    df_estados = df_estados.rename_axis(index = "ESTADOS")
-
-    # Plotagem do gráfico
-    regioes_cores = {"Norte": (["AC", "AM", "AP", "PA", "RO", "RR", "TO"], "green"),
-                    "Nordeste": (["AL", "BA", "CE", "MA", "PB", "PE", "PI", "RN", "SE"], "red"),
-                    "Centro-Oeste": (["DF", "GO", "MS", "MT"], "orange"),
-                    "Sudeste": (["ES", "MG", "RJ", "SP"], "yellow"),
-                    "Sul": (["PR", "RS", "SC"], (1, 0, 1))}
-
-    barras = plt.barh(df_estados.index, df_estados["QUANTIDADE"], height = 0.5)
-    total_soma = df_estados["QUANTIDADE"].sum()
-
-    for num, barra in enumerate(barras):
-        estado = df_estados.index[num]
-        for regiao, (siglas, cor) in regioes_cores.items():
-            if estado in siglas:
-                barra.set_color(cor)
-
-        largura = barra.get_width()
-        porcentagem = (largura / total_soma) * 100
-        if porcentagem < 10:
-            plt.text(largura + 10, barra.get_y() + barra.get_height() / 2, f"{porcentagem:.2f}%", ha = "left",
-                    va = "center", fontsize=8, fontweight = "bold", color = "#010101", linespacing = 0.9)
-        else:
-            plt.text(largura - 70, barra.get_y() + barra.get_height() / 2, f"{porcentagem:.2f}%", ha = "center",
-                    va = "center", fontsize=8, fontweight = "bold", color = "#010101", linespacing = 0.9)
-
-    # Legenda
-    for regiao, (siglas, cor) in regioes_cores.items():
-        plt.bar(0, 0, color = cor, label = regiao)
-    plt.legend(loc = "lower right")
-
-    # Personalização do gráfico
-    plt.xlabel("Quantidade")
-    plt.ylabel("Estados")
-    plt.title("Quantidade de Apreensões por Estado")
-    plt.grid(axis = "x", linestyle = "--", alpha=0.6)
-
-    return plt
-
 if __name__ == "__main__":
     df = clean_data(collect_data())
 
+    fig_vinicius = make_plot_vinicius(df)
+    fig_vinicius.savefig('graficos/graficovinicius.png')
+
     fig_guilherme = make_plot_guilherme(df)
-    fig_guilherme.show()
     fig_guilherme.savefig('graficos/graficoguilherme.png')
 
     fig_gustavo = make_gustavo_plot(df)
-    fig_gustavo.show()
     fig_gustavo.savefig('graficos/graficogustavo.png')
 
     fig_joao = make_plot_joao(df)
-    fig_joao.show()
     fig_joao.savefig('graficos/graficojoao.png')
-
-    fig_vinicius = make_plot_vinicius(df)
-    fig_vinicius.show()
-    fig_vinicius.savefig('graficos/graficovinicius.png')
